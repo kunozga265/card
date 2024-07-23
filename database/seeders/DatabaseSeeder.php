0523711b5ec0977458d73f05be3685be110f21f6
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2024-07-23 13:58:37 +0200

    User Table Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
        $this->call(PageGroupTableSeeder::class);
        $this->call(ProjectTableSeeder::class);
+       $this->call(UserTableSeeder::class);
     }
 }
